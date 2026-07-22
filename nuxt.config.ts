8eb882034ea702bc4c9f2661be171fd615c7b739
Meenakshi Shekhawat <meenakshishekhawat2003@gmail.com>
2026-07-22 18:21:33 +0530

    fix: move PostCSS config into nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,31 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
+
   modules: ['@nuxtjs/tailwindcss'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
+
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
-      ]
-    }
-  }
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg',
+        },
+      ],
+    },
+  },
 })
-
